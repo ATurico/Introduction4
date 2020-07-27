40cf63ecb81802df50a0c5c6a68f7da5590f072a
ATurico <tostrom@outlook.com>
2020-07-27 17:17:00 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+number=float(input("Type a number: "))
+if number < 10:
+  print("that number is less then ten")
+elif number == 10:
+  print("that number is ten")
+else:
+  print("That number is greater then ten")
